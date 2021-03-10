31c6fb72c24c450130ff6d7b0dbc0cbf274665ca
Luis Manuel Torres Treviño <luis.trevino@drumbot.com.mx>
2021-03-09 19:59:34 -0600

    Create the test data for the tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,12 +2,33 @@ import unittest
 import json
 
 from app import app, db
+from app.api.controllers.user_controller import UserController
+from app.api.controllers.company_controller import CompanyController
 from app.api.controllers.encrypt_password import encrypt_password, check_password
 
 class LoginTest(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        db.create_all()
+
+        controller = UserController()
+        company_controller = CompanyController()
+        
+        compan = company_controller.add_company({
+            "name": "test",
+            "description": ""
+        })
+
+        user = controller.new_user(
+            username="admin",
+            mail="test@test.com",
+            first_name="user",
+            last_name="test",
+            password="123123",
+            company=1
+        )
+
     
     def test_successful_signup(self):
         payload = json.dumps({
