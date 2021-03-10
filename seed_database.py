569cddc59a952ccde197f0350083dc174971edd8
Luis Manuel Torres Treviño <luis.trevino@drumbot.com.mx>
2021-03-09 21:56:09 -0600

    Adding a new file with the database seed

---
+++ seed_database.py
@@ -0,0 +1,51 @@
+from alembic import op
+
+from app.models.users import User
+from app.models.company import Company
+
+
+def insert_data():
+    company_data = {
+        "name": "Test Company",
+        "description": ""
+    }
+
+    company = Company(company_data["name"], company_data["description"])
+    company.save()
+    id = Company.query(id).filter(name=company_data["name"]).first()
+
+    user_data = {
+        "username": "admin",
+        "password": "123123",
+        "first_name": "test",
+        "last_name": "user",
+        "email": "test@test.com",
+        "company": id if id else Company.query(id).filter(name=company_data["name"]).first()
+    }
+
+    other_user_data = {
+        "username": "admin2",
+        "password": "123123",
+        "first_name": "test",
+        "last_name": "user",
+        "email": "test@test.com",
+        "company": id if id else Company.query(id).filter(name=company_data["name"]).first()
+    }
+
+    user = User(
+        user_data["username"],
+        user_data["email"],
+        user_data["first_name"],
+        user_data["last_name"],
+        user_data["password"],
+        user_data["company"]
+    ).save()
+    
+    other_user_data = User(
+        other_user_data["username"],
+        other_user_data["email"],
+        other_user_data["first_name"],
+        other_user_data["last_name"],
+        other_user_data["password"],
+        other_user_data["company"]
+    ).save()
